4372c6f07cdbe84e67545e852617c5253f3041b8
Oliver Thane <othane@gmail.com>
2013-07-22 09:38:56 +1200

    a clean but broken way to set red led
    this is how I think we should set the red LED properly, ie
    request_region, ioport_map, and then ioread16 the reg before iowrite16
    the updated value, then cleaning all that up on exit, However it does
    not work :( ... here is a terminal output of the insmod:
    
    root@ts4700:~/dev# insmod ./lcd.ko
    Segmentation fault
    
    root@ts4700:~/dev# dmesg
    lcd: module license 'unspecified' taints kernel.
    LCD POC driver started
    requesting red output region
    ioport_map red output region
    calling set red
    Unable to handle kernel paging request at virtual address 80004012
    pgd = c269c000
    [80004012] *pgd=00000000
    Internal error: Oops: 5 [#1] PREEMPT
    Modules linked in: lcd(P+) bmm snd_soc_ts4700 snd_soc_pxa3xx snd_soc_sgtl5000 snd_soc_pxa3xx_ssp snd_soc_core snd_pcm snd_timer snd_page_alloc snd
    CPU: 0    Tainted: P            (2.6.29-ts4700-00 #1)
    PC is at set_red+0x18/0x44 [lcd]
    LR is at init_module+0xb0/0x10c [lcd]
    pc : [<bf075018>]    lr : [<bf07516c>]    psr: 60000013
    sp : c2679ed0  ip : c2679ee8  fp : c2679ee4
    r10: 00000000  r9 : c2678000  r8 : 00000000
    r7 : bf0750bc  r6 : 00012018  r5 : 00000000  r4 : bf07577c
    r3 : 80004012  r2 : 00020000  r1 : ce0cbbe0  r0 : bf0751ec
    Flags: nZCv  IRQs on  FIQs on  Mode SVC_32  ISA ARM  Segment user
    Control: 0405397f  Table: 0269c018  DAC: 00000015
    Process insmod (pid: 9229, stack limit = 0xc2678270)
    Stack: (0xc2679ed0 to 0xc267a000)
    9ec0:                                     bf07577c 00000000 c2679f04 c2679ee8
    9ee0: bf07516c bf075010 00000000 0000000f 00000eb1 bf075674 c2679f7c c2679f08
    9f00: c00272f4 bf0750cc c269d000 bf0756bc 00000200 bf075578 d0902a08 bf075680
    9f20: 00000011 d0902a30 00000000 00000000 00000000 00000000 00000000 00000000
    9f40: c0340b7c 00000eb1 bf075674 00012018 00000000 c0027fe8 c2679f7c 00000eb1
    9f60: bf075674 00012018 00000000 c0027fe8 c2679fa4 c2679f80 c006aad8 c00272a8
    9f80: ffffffff be901da9 00000000 be901da9 00012008 00000080 00000000 c2679fa8
    9fa0: c0027e40 c006aa50 00000000 be901da9 00012018 00000eb1 00012008 0000002e
    9fc0: 00000000 be901da9 00012008 00000080 00004000 00000000 00000003 00000000
    9fe0: 00000eb1 be901b1c 00008cdc 400ee1d4 60000010 00012018 00000000 00000000
    Backtrace:
    [<bf075000>] (set_red+0x0/0x44 [lcd]) from [<bf07516c>] (init_module+0xb0/0x10c [lcd])
     r5:00000000 r4:bf07577c
    [<bf0750bc>] (init_module+0x0/0x10c [lcd]) from [<c00272f4>] (__exception_text_end+0x5c/0x1a4)
     r5:bf075674 r4:00000eb1
    [<c0027298>] (__exception_text_end+0x0/0x1a4) from [<c006aad8>] (sys_init_module+0x98/0x198)
     r8:c0027fe8 r7:00000000 r6:00012018 r5:bf075674 r4:00000eb1
    [<c006aa40>] (sys_init_module+0x0/0x198) from [<c0027e40>] (ret_fast_syscall+0x0/0x2c)
     r7:00000080 r6:00012008 r5:be901da9 r4:00000000
    Code: e24cb004 e59f3020 e59f0020 e5933000 (e1d340b0)
    ---[ end trace 1066d7eaecdb0fdb ]---
    
    It looks like the calls to request_region and ioport_map succeed but the
    ioread16 fails ??

--- lcd.c
+++ lcd.c
@@ -4,17 +4,95 @@
  */
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/ioport.h>
+#include <asm/io.h>
+
+#define SYSCON_BASE (0x80004000)
+
+#define E_IN_BASE  (SYSCON_BASE + 0x26)
+#define E_OUT_BASE (SYSCON_BASE + 0x16)
+#define E_DIR_BASE (SYSCON_BASE + 0x1e)
+
+#define RED_OUT_BASE (SYSCON_BASE + 0x12)
+static void __iomem *red_out_base = NULL;
+#define RED_BIT (1<<12)
+
+
+static void set_red(int red)
+{
+	unsigned short val = ioread16(red_out_base);
+	
+	printk(KERN_INFO "set red\n");
+
+	if (val & RED_BIT)
+		printk(KERN_INFO "red led if off\n");
+	else
+		printk(KERN_INFO "red led if on\n");
+
+	// set red on
+	if (red)
+		val |= RED_BIT;
+	else
+		val &= ~RED_BIT;
+	iowrite16(red_out_base, val);
+}
 
 int lcd_init(void)
 {
+	int ret = 0;
+
 	// dbg start up msg
 	printk(KERN_INFO "LCD POC driver started\n");
 
+	// grab control over the memory for the io pins
+	printk(KERN_INFO "requesting red output region\n");
+	if (!request_region(RED_OUT_BASE, 2, "lcd"))
+	{
+		printk(KERN_ERR "%s: requested I/O region (%#x:2) is "
+			   "in use\n", "lcd, RED_OUT_BASE", RED_OUT_BASE);
+		ret = -ENODEV;
+		goto out1;
+	}
+
+	printk(KERN_INFO "ioport_map red output region\n");
+	red_out_base = ioport_map(RED_OUT_BASE, 2);
+	if (!red_out_base)
+	{
+		printk(KERN_ERR "%s: requested I/O region (%#x:2) is "
+			   "in use\n", "lcd, RED_OUT_BASE", RED_OUT_BASE);
+		ret = -EIO;
+		goto out2;
+	}
+	
+	// just set the some output for now
+	printk(KERN_INFO "calling set red\n");
+	set_red(1);
+
 	return 0;
+
+	// bail out section
+	printk(KERN_INFO "bailing out !\n");
+out2:
+	if (red_out_base)
+		ioport_unmap(red_out_base);
+	red_out_base = NULL;
+	release_region(RED_OUT_BASE, 2);
+out1:
+	return ret;
 }
 
 void lcd_cleanup(void)
 {
+	// clear the e pin so we can retest
+	printk(KERN_INFO "calling clear red\n");
+	set_red(0);
+	
+	// clear resources
+	printk(KERN_INFO "freeing resources\n");
+	if (red_out_base)
+		ioport_unmap(red_out_base);
+	red_out_base = NULL;
+	release_region(RED_OUT_BASE, 2);
 	printk(KERN_INFO "LCD POC driver done\n");
 }
 
