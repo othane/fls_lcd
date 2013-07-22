27e1cde4971833c84554f5eabe0cec69336c2cd1
Oliver Thane <othane@gmail.com>
2013-07-22 12:32:34 +1200

    add full output control over e pin
    this removes the initial stuffing around with the red io pin and
    actually sets/clears the lcd e pin (including selecting the direction)

--- lcd.c
+++ lcd.c
@@ -13,90 +13,135 @@
 #define E_OUT_BASE (SYSCON_BASE + 0x16)
 #define E_DIR_BASE (SYSCON_BASE + 0x1e)
 
-#define RED_OUT_BASE (SYSCON_BASE + 0x12)
-static void __iomem *red_out_base = NULL;
-#define RED_BIT (1<<12)
+static void __iomem *e_dir_base = NULL;
+static void __iomem *e_out_base = NULL;
+#define E_BIT (1<<8)
 
 
-static void set_red(int state)
+static void set_e_dir(int dir)
 {
-	unsigned short val = ioread16(red_out_base);
+	unsigned short val = ioread16(e_dir_base);
 	
-	printk(KERN_INFO "set red\n");
+	printk(KERN_INFO "set e dir\n");
 
-	if (val & RED_BIT)
-		printk(KERN_INFO "red led is on\n");
+	// dbg - print old state
+	if (val & E_BIT)
+		printk(KERN_INFO "e dir is output\n");
 	else
-		printk(KERN_INFO "red led is off\n");
+		printk(KERN_INFO "e dir is input\n");
+	
+	// set dir
+	if (dir)
+		val |= E_BIT;
+	else
+		val &= ~E_BIT;
+	iowrite16(val, e_dir_base);
+}
 
-	// set red on
-	if (state)
-		val |= RED_BIT;
+static void set_e(int e)
+{
+	unsigned short val = ioread16(e_out_base);
+	
+	printk(KERN_INFO "set e\n");
+
+	// dbg - print old state
+	if (val & E_BIT)
+		printk(KERN_INFO "e is hi\n");
 	else
-		val &= ~RED_BIT;
-	iowrite16(val, red_out_base);
+		printk(KERN_INFO "e is lo\n");
+	
+	// set dir
+	if (e)
+		val |= E_BIT;
+	else
+		val &= ~E_BIT;
+	iowrite16(val, e_out_base);
 }
 
 int lcd_init(void)
 {
 	int ret = 0;
-	unsigned int val;
 
 	// dbg start up msg
 	printk(KERN_INFO "LCD POC driver started\n");
 
 	// grab control over the memory for the io pins
-	printk(KERN_INFO "requesting red output region\n");
-	if (!request_region(RED_OUT_BASE, 2, "lcd"))
+	printk(KERN_INFO "requesting E dir region\n");
+	if (!request_region(E_DIR_BASE, 2, "lcd"))
 	{
 		printk(KERN_ERR "%s: requested I/O region (%#x:2) is "
-			   "in use\n", "lcd, RED_OUT_BASE", RED_OUT_BASE);
+			   "in use\n", "lcd, E_DIR_BASE", E_DIR_BASE);
 		ret = -ENODEV;
 		goto out1;
 	}
-
-	printk(KERN_INFO "ioremap red output region\n");
-	red_out_base = ioremap_nocache(RED_OUT_BASE, 2);
-	if (!red_out_base)
+	printk(KERN_INFO "requesting E out region\n");
+	if (!request_region(E_OUT_BASE, 2, "lcd"))
 	{
-		printk(KERN_ERR "%s: map I/O region (%#x:2) is "
-			   "in use\n", "lcd, RED_OUT_BASE", RED_OUT_BASE);
-		ret = -EIO;
+		printk(KERN_ERR "%s: requested I/O region (%#x:2) is "
+			   "in use\n", "lcd, E_OUT_BASE", E_OUT_BASE);
+		ret = -ENODEV;
 		goto out2;
 	}
-	printk(KERN_INFO "ioremap red output region from 0x%.8x to 0x%.8x\n", RED_OUT_BASE, (int)red_out_base);
-	
-	// just set the some output for now
-	printk(KERN_INFO "calling read16 red\n");
-	val = ioread16(red_out_base);
-	printk(KERN_INFO "I read 0x%x back\n", val);
-	set_red(1);
 
+	printk(KERN_INFO "ioremap E dir region\n");
+	e_dir_base = ioremap_nocache(E_DIR_BASE, 2);
+	if (!e_dir_base)
+	{
+		printk(KERN_ERR "%s: map I/O region (%#x:2) is "
+			   "in use\n", "lcd, E_DIR_BASE", E_DIR_BASE);
+		ret = -EIO;
+		goto out3;
+	}
+	printk(KERN_INFO "ioremap E dir region from 0x%.8x to 0x%.8x\n", E_DIR_BASE, (int)e_dir_base);
+	
+	printk(KERN_INFO "ioremap E out region\n");
+	e_out_base = ioremap_nocache(E_OUT_BASE, 2);
+	if (!e_dir_base)
+	{
+		printk(KERN_ERR "%s: map I/O region (%#x:2) is "
+			   "in use\n", "lcd, E_OUT_BASE", E_OUT_BASE);
+		ret = -EIO;
+		goto out3;
+	}
+	printk(KERN_INFO "ioremap E out region from 0x%.8x to 0x%.8x\n", E_OUT_BASE, (int)e_out_base);
+
+	// set e pin to output mode hi
+	set_e_dir(1);
+	set_e(1);
+	
 	return 0;
 
 	// bail out section
+out3:
 	printk(KERN_INFO "bailing out !\n");
+	if (e_out_base)
+		ioport_unmap(e_out_base);
+	e_out_base = NULL;
+	if (e_dir_base)
+		ioport_unmap(e_dir_base);
+	e_dir_base= NULL;
+	release_region(E_OUT_BASE, 2);
 out2:
-	if (red_out_base)
-		ioport_unmap(red_out_base);
-	red_out_base = NULL;
-	release_region(RED_OUT_BASE, 2);
+	release_region(E_DIR_BASE, 2);
 out1:
 	return ret;
 }
 
 void lcd_cleanup(void)
 {
-	// clear the e pin so we can retest
-	printk(KERN_INFO "calling clear red\n");
-	set_red(0);
-	
+	// reset e
+	set_e(0);
+
 	// clear resources
 	printk(KERN_INFO "freeing resources\n");
-	if (red_out_base)
-		ioport_unmap(red_out_base);
-	red_out_base = NULL;
-	release_region(RED_OUT_BASE, 2);
+	if (e_out_base)
+		ioport_unmap(e_out_base);
+	e_out_base = NULL;
+	if (e_dir_base)
+		ioport_unmap(e_dir_base);
+	e_dir_base = NULL;
+	release_region(E_OUT_BASE, 2);
+	release_region(E_DIR_BASE, 2);
 	printk(KERN_INFO "LCD POC driver done\n");
 }
 
