da41755d3f9a105552aac2e4cef88677754809f2
Oliver Thane <othane@gmail.com>
2013-07-25 01:03:06 +1200

    add basic dio functionality
    init dio so that we have: a direction (dir), input (in), and output (out)
    registers; reading or writing to the vaddr will read or set the fpga dio
    pins
    
    also definitions for the lcd pins RS, RW, E, D4:7 have been added and
    checked that all pins can be set hi and lo, I also checked D4:7 can read
    values from the in register

--- fls_lcd.c
+++ fls_lcd.c
@@ -3,17 +3,143 @@
  */
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/ioport.h>
+#include <asm/io.h>
+
+#define MODULE_NAME "FLS front panel LCD"
+#define SYSCON_BASE (0x80004000)
+#define RS	(1 << 6)
+#define RW	(1 << 7)
+#define E	(1 << 8)
+#define D4	(1 << 0)
+#define D5	(1 << 1)
+#define D6	(1 << 4)
+#define D7	(1 << 5)
+
+struct dio_reg_t {
+	unsigned long paddr;
+	size_t size;
+	struct resource *res;
+	void __iomem *vaddr;
+};
+
+static struct dio_t {
+	struct dio_reg_t dir;	
+	struct dio_reg_t in;	
+	struct dio_reg_t out;	
+} lcd_dio = {
+	.dir = {.paddr = SYSCON_BASE + 0x1e, .size = 2},
+	.in  = {.paddr = SYSCON_BASE + 0x26, .size = 2},
+	.out = {.paddr = SYSCON_BASE + 0x16, .size = 2},
+};
+
+
+static int dio_init(struct dio_t *dio)
+{
+	// sanity check (can remove after devel phase)
+	if (!dio) {
+		printk(KERN_ERR "invalid parameter\n");
+		return -EFAULT;
+	}
+
+	// request dir, in, out regions
+	dio->dir.res = request_region(dio->dir.paddr, dio->dir.size, MODULE_NAME);
+	if (!dio->dir.res) {
+		printk(KERN_ERR "requested io region (%.8lx) is in use\n", dio->dir.paddr);
+		return -EBUSY;
+	}
+	dio->in.res = request_region(dio->in.paddr, dio->in.size, MODULE_NAME);
+	if (!dio->in.res) {
+		printk(KERN_ERR "requested io region (%.8lx) is in use\n", dio->in.paddr);
+		return -EBUSY;
+	}
+	dio->out.res = request_region(dio->out.paddr, dio->out.size, MODULE_NAME);
+	if (!dio->out.res) {
+		printk(KERN_ERR "requested io region (%.8lx) is in use\n", dio->out.paddr);
+		return -EBUSY;
+	}
+	
+	// get virtual address of dir, in, out from phys address so we may use them
+	dio->dir.vaddr = ioremap_nocache(dio->dir.paddr, dio->dir.size);
+	if (!dio->dir.vaddr) {
+		printk(KERN_ERR "unable to remap io region (%.8lx)\n", dio->dir.paddr);
+		return -EFAULT;
+	}
+	dio->in.vaddr = ioremap_nocache(dio->in.paddr, dio->in.size);
+	if (!dio->in.vaddr) {
+		printk(KERN_ERR "unable to remap io region (%.8lx)\n", dio->in.paddr);
+		return -EFAULT;
+	}
+	dio->out.vaddr = ioremap_nocache(dio->out.paddr, dio->out.size);
+	if (!dio->out.vaddr) {
+		printk(KERN_ERR "unable to remap io region (%.8lx)\n", dio->out.paddr);
+		return -EFAULT;
+	}
+	
+	return 0;
+}
+
+static void dio_deinit(struct dio_t *dio)
+{
+	// unmap virtual addresses of dir, in, out
+	if (dio->dir.vaddr)
+		iounmap(dio->dir.vaddr);
+	dio->dir.vaddr = NULL;
+	if (dio->in.vaddr)
+		iounmap(dio->in.vaddr);
+	dio->in.vaddr = NULL;
+	if (dio->out.vaddr)
+		iounmap(dio->out.vaddr);
+	dio->out.vaddr = NULL;
+
+	// release memory regions
+	if (dio->dir.res)
+		release_region(dio->dir.paddr, dio->dir.size);
+	dio->dir.res = NULL;
+	if (dio->in.res)
+		release_region(dio->in.paddr, dio->in.size);
+	dio->in.res = NULL;
+	if (dio->out.res)
+		release_region(dio->out.paddr, dio->out.size);
+	dio->out.res = NULL;
+}
 
 int lcd_init(void)
 {
+	int ret = 0;
+	uint16_t val;
+
 	// start up msg
 	printk(KERN_INFO "FLS LCD driver started\n");
 
+	// init the registers etc
+	ret = dio_init(&lcd_dio);
+	if (ret < 0) {
+		printk(KERN_ERR "lcd module unable to init dio, bailing out\n");
+		goto fail;
+	}
+
+	// set RS, RW, and E as lo outputs (these remain outputs throughout lcd operation)
+	val = ioread16(lcd_dio.out.vaddr);
+	val &= ~(RS | RW | E);
+	iowrite16(val, lcd_dio.out.vaddr);
+	val = ioread16(lcd_dio.dir.vaddr);
+	val |= (RS | RW | E);
+	iowrite16(val, lcd_dio.dir.vaddr);
+
 	return 0;
+
+fail:
+	// deinit registers etc
+	dio_deinit(&lcd_dio);
+	return ret;
 }
 
 void lcd_cleanup(void)
 {
+	// deinit registers etc
+	dio_deinit(&lcd_dio);
+
 	// shutdown msg
 	printk(KERN_INFO "FLS LCD driver done\n");
 }
